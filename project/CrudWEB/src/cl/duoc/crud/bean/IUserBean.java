29357a20371cc60ec98c9d8eb13970e700a0490c
carlos apablaza <c.apablaza.z@gmail.com>
2014-03-25 23:53:29 -0300

    c:foreach example

--- project/CrudWEB/src/cl/duoc/crud/bean/IUserBean.java
+++ project/CrudWEB/src/cl/duoc/crud/bean/IUserBean.java
@@ -1,5 +1,7 @@
 package cl.duoc.crud.bean;
 
+import java.util.List;
+
 import cl.duoc.crud.to.UserTO;
 
 public interface IUserBean {
@@ -7,5 +9,7 @@ public interface IUserBean {
 	public String userSaludo();
 	
 	public UserTO getMyUser();
+	
+	public List<UserTO> getUsers();
 	
 }
--- project/CrudWEB/src/cl/duoc/crud/dao/impl/UserDAO.java
+++ project/CrudWEB/src/cl/duoc/crud/dao/impl/UserDAO.java
@@ -1,5 +1,8 @@
 package cl.duoc.crud.dao.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cl.duoc.crud.dao.IUserDAO;
 import cl.duoc.crud.to.UserTO;
 
@@ -17,6 +20,40 @@ public class UserDAO implements IUserDAO{
 		user.setName("carlos");
 		user.setEdad(21);
 		return user;
+	}
+
+	
+	public List<UserTO> getUsers() {
+		List<UserTO> users = new ArrayList<UserTO>();
+		UserTO userTO = new UserTO();
+		
+		userTO.setName("carlos");
+		userTO.setEdad(27);
+		
+		users.add(userTO);
+		
+		userTO = new UserTO();
+		
+		userTO.setName("jose");
+		userTO.setEdad(99);
+		
+		users.add(userTO);
+		
+		userTO = new UserTO();
+		
+		userTO.setName("pedro");
+		userTO.setEdad(null);
+		
+		users.add(userTO);
+		
+		userTO = new UserTO();
+		userTO.setName("marcelo");
+		userTO.setEdad(100);
+				
+		users.add(userTO);
+		
+		return users;
+		
 	}
 
 }
